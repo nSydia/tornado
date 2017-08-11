d973dc35e9083d82bdc719a5b2e4bd8d797d1a96
Magdy M. Ibrahim <sakafa_magdy@yahoo.com>
2017-08-10 22:19:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 apt-get install dsniff etherape nmap ettercap-graphical iptraf-ng driftnet tshark tcpdump sslstrip mitmproxy -y
 pip install mitmproxy
 apt-get install build-essential python-dev libnetfilter-queue-dev -y
+git clone https://github.com/LeonardoNve/dns2proxy
+git clone https://github.com/byt3bl33d3r/sslstrip2
 cd sslstrip2
 python setup.py install
 clear
